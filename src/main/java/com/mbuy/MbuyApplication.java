4e376113a46135b91f037ba241e040442ce5d736
GitHubxd <375237163@qq.com>
2018-08-21 20:59:54 +0800

    增加mybatis druid

--- src/main/java/com/mbuy/MbuyApplication.java
+++ src/main/java/com/mbuy/MbuyApplication.java
@@ -1,9 +1,11 @@
 package com.mbuy;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.mbuy.mapper")
 public class MbuyApplication {
 
 	public static void main(String[] args) {
