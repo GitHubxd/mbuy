60dfe03fe3c29118c0fff926f520d0bb5dfc958c
GitHubxd <375237163@qq.com>
2018-08-21 17:39:10 +0800

    basic

---
+++ src/main/java/com/mbuy/MbuyApplication.java
@@ -0,0 +1,12 @@
+package com.mbuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbuyApplication.class, args);
+	}
+}
